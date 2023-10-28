b274237e0235c20404d6d04d3276e9ed1ae8915a
sameembb <149255942+sameembb@users.noreply.github.com>
2023-10-28 21:13:59 +0530

    Add files via upload

---
+++ linear search.py
@@ -0,0 +1,12 @@
+def linearSearchProduct(productList, targetProduct):
+   indices = [] 
+   for index, product in enumerate(productList):
+     if product == targetProduct:
+       indices.append(index)
+   return indices
+
+product = ["shoes", "boot", "loafer", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(product, target)
+print(result)
